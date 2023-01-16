bcb6ab59915a59ad13941391170f98320d68c903
Irei <IREI@example.com>
2023-01-16 22:07:50 +0300

    task25

---
+++ c#/task25/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+int powForNumber(int yourNumber, int index)
+{
+    int total = 1;
+    for (int i = 0; i < index; i++)
+    {
+        total = total * yourNumber;
+    }
+    return total;
+}
+void showResult(string message, int result)
+{    
+    Console.WriteLine($"{message} {result}");
+}
+int number = getUserData("Какое число будем возводить в степень? ");
+int degree = getUserData($"Число {number} в какую степень возводим? ");
+int degreeNumber = powForNumber(number, degree);
+showResult($"Ответ: {number} ^ {degree} =", degreeNumber);
