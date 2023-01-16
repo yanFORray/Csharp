fc31c887768d2f4095dd9ff76094b99a6c40a9c3
Irei <IREI@example.com>
2023-01-16 16:56:02 +0300

    task24

---
+++ c#/task24/Program.cs
@@ -0,0 +1,25 @@
+﻿// число А на вход  выводит сумму чисел от 1 до А
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.Write(data);
+}
+int end = getUserData("Введите число А для получения суммы");
+int sum = getSumOfRange(1,end+1);
+showData($"сумму чисел от 1 до {end} = ",sum );
